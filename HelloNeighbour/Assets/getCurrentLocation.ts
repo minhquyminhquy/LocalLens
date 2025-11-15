505917d94d3052cf8937cf62657264f30dec88c4
Quy Le <quy.le@tuni.fi>
2025-11-15 11:40:59 +0200

    feat: get glass location

--- HelloNeighbour/Assets/getCurrentLocation.ts
+++ HelloNeighbour/Assets/getCurrentLocation.ts
@@ -1,6 +1,60 @@
+require('LensStudio:RawLocationModule');
+
 @component
-export class NewScript extends BaseScriptComponent {
-    onAwake() {
-        print("Hello world");
-    }
+export class LocationExample extends BaseScriptComponent {
+  latitude: number;
+  longitude: number;
+  altitude: number;
+  horizontalAccuracy: number;
+  verticalAccuracy: number;
+  timestamp: Date;
+  locationSource: string;
+
+  private repeatUpdateUserLocation: DelayedCallbackEvent;
+  private locationService: LocationService;
+  onAwake() {
+    this.createEvent('OnStartEvent').bind(() => {
+      this.createAndLogLocationAndHeading();
+    });
+
+    this.repeatUpdateUserLocation = this.createEvent('DelayedCallbackEvent');
+    this.repeatUpdateUserLocation.bind(() => {
+      this.locationService.getCurrentPosition(
+        function (geoPosition) {
+          if (
+            this.timestamp === undefined ||
+            this.timestamp.getTime() != geoPosition.timestamp.getTime()
+          ) {
+            this.latitude = geoPosition.latitude;
+            this.longitude = geoPosition.longitude;
+            this.horizontalAccuracy = geoPosition.horizontalAccuracy;
+            this.verticalAccuracy = geoPosition.verticalAccuracy;
+            print('long: ' + this.longitude);
+            print('lat: ' + this.latitude);
+            this.timestamp = geoPosition.timestamp;
+          }
+        },
+        function (error) {
+          print(error);
+        }
+      );
+      this.repeatUpdateUserLocation.reset(1.0);
+    });
+  }
+  createAndLogLocationAndHeading() {
+    this.locationService = GeoLocation.createLocationService();
+    this.locationService.accuracy = GeoLocationAccuracy.Navigation;
+
+    var onOrientationUpdate = function (northAlignedOrientation) {
+      let heading = GeoLocation.getNorthAlignedHeading(northAlignedOrientation);
+      //print('Heading orientation: ' + heading.toFixed(3));
+      var rotation = (heading * Math.PI) / 180;
+      //print('Screen transform rotation: ' + quat.fromEulerAngles(0, 0, rotation));
+    };
+    this.locationService.onNorthAlignedOrientationUpdate.add(
+      onOrientationUpdate
+    );
+
+    this.repeatUpdateUserLocation.reset(0.0);
+  }
 }
